d501d404bd3d627352e167f2fe90e2cea9be4c99
kmsumanthetechcloud <suman.kumar@thetechclouds.com>
2025-02-26 15:08:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-def add_numbers(a, b):
-    return a + b
+from flask import Flask
 
-if __name__ == "__main__":
-    print("Sum:", add_numbers(5, 3))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Jenkins Deployment with Docker!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
