f076dfa7d55e4dc1cfe6e5df3d5e8d0cfc776af5
kmsumanthetechcloud <suman.kumar@thetechclouds.com>
2025-02-26 13:27:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, Flask is running!"
+def add_numbers(a, b):
+    return a + b
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    print("Sum:", add_numbers(5, 3))
